684dc66b7be3247ebbf682c9181e12e2c42d0687
Jun Wang <andmap.co@gmail.com>
2020-08-01 16:50:26 -0700

    feat: reconciliation

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,16 @@ import { Counter } from "./Counter";
 import { Chats } from "./Chats";
 import { Hello } from "./Hello";
 import { Forms } from "./Forms";
-import { hasPairWithSum } from "./utils/helpers";
+import {
+  hasPairWithSum,
+  fibonacciBottomUp,
+  reconciliation,
+} from "./utils/helpers";
 import Memo from "./Memo";
 const App = () => {
+  const data1 = [5.1, 1, 1.1, 2.3, 3.0, 5.2, 5.1, 3.0];
+  const data2 = [1.1, 3.0, 5.1, 3.1, 5.1, 8.1];
+
   const array = useMemo(() => {
     return ["one", "2"];
   }, []);
@@ -25,6 +32,14 @@ const App = () => {
       <h1>
         sum: {hasPairWithSum([1, 2, 3, 9], 8) === true ? "true" : "false"}
       </h1>
+      <h1>fibonacciBottomUp(6): {fibonacciBottomUp(6)}</h1>
+      <h1>fibonacciBottomUp(20): {fibonacciBottomUp(20)}</h1>
+      <h1>
+        reconciliation( <br />
+        {data1.toString()},<br />
+        {data2.toString()})
+      </h1>
+      <div>{reconciliation(data1, data2).toString()}</div>
       <Forms />
       <Hello />
       <Counter initialCount={0} />
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,3 +13,69 @@ export const hasPairWithSum = (data: number[], sum: number) => {
   }
   return false;
 };
+
+export const hasPairWithSumUseMap = (data: number[], sum: number) => {
+  /*
+   * [1, 2, 4, 4], 8 = true
+   * [1, 2, 3, 9], 8 = false
+   */
+
+  let comp = new Map();
+  for (let i = 0; i < data.length; i++) {
+    const num = data[i];
+    if (comp.has(num)) {
+      return true;
+    } else {
+      comp.set(sum - num, num);
+    }
+  }
+  return false;
+};
+
+export const fibonacciBottomUp = (n: number) => {
+  /*
+   * [1, 1, 2, 3, 5, 8]
+   * fibonacciBottomUp(6) = 8
+   */
+
+  let bottomUp: number[] = [];
+  if (n <= 2) return 1;
+  else {
+    bottomUp.push(1);
+    bottomUp.push(1);
+    for (let i = 3; i <= n; i++) {
+      bottomUp[i - 1] = bottomUp[i - 2] + bottomUp[i - 3];
+    }
+    return bottomUp[n - 1];
+  }
+};
+
+export const reconciliation = (data1: number[], data2: number[]) => {
+  /*
+   * data1 = [5.1, 1, 1.1, 2.3, 3.0, 5.2, 5.1, 3.0]
+   * data2 = [1.1, 3.0, 5.1, 3.1, 5.1, 8.1]
+   * reconciliation = [1.1, 3.0, 5.1]
+   */
+
+  let comp = new Map();
+  let result = [];
+  for (let i = 0; i < data1.length; i++) {
+    const num = data1[i];
+    if (!comp.has(num)) {
+      comp.set(num, 1);
+    } else {
+      comp.set(num, comp.get(num) + 1);
+    }
+  }
+
+  for (let i = 0; i < data2.length; i++) {
+    const num = data2[i];
+    if (comp.has(num)) {
+      result.push(num);
+      const count = comp.get(num) - 1;
+      if (count === 0) comp.delete(num);
+      else comp.set(num, count);
+    }
+  }
+  return result;
+};
