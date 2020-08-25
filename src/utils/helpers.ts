e0a2714c870bdcfb8ca4d5dc0ad68ead2dd82d60
Jun Wang <andmap.co@gmail.com>
2020-08-24 23:44:53 -0700

    feat: mergeSort

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -131,3 +131,37 @@ function inventoryList() {
 
 const obj = inventoryList();
 console.log(obj.add("Testing"));
+
+const mergeSort = (unsortedArray: number[]) => {
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
+  }
+  const middle = Math.floor(unsortedArray.length / 2);
+  const left = unsortedArray.slice(0, middle);
+  const right = unsortedArray.slice(middle);
+  const result = merge(mergeSort(left), mergeSort(right));
+  return result;
+};
+
+const merge = (left: number[], right: number[]) => {
+  let resultArray: number[] = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  // We will concatenate values into the resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+
+  // We need to concat here because there will be one element remaining
+  // from either left OR the right
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex)) as number[];
+};
